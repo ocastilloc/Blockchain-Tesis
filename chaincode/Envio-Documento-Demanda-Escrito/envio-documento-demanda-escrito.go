e776346a0c7f047d630f3553c3b0c2519c51d276
pjud-blockchain <oscar.rodrigo.castillo@gmail.com>
2021-02-18 23:18:55 -0300

    configuraciones blockchain pjud

---
+++ chaincode/Envio-Documento-Demanda-Escrito/envio-documento-demanda-escrito.go
@@ -0,0 +1,110 @@
+package main
+
+//Importar librerias
+//api de contratos inteligentes nuevo patron de diseño de 
+//contratos inteligentes
+//"github.com/hyperledger/fabric-contract-api-go/contractapi"
+
+import (
+        "encoding/json"
+        "fmt"                                                       
+        "github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+//Definicion de estructuras
+//Forma de usar la estructura de contratos inteligentes
+type SmartContract struct {
+        contractapi.Contract
+}
+
+//Crear un conjunto de estructuras
+//Definir activos Demandante, TipoDocumento y ContenidoDocumento
+
+type Documento struct {
+        Demandante  string `json:"demandante"`
+        TipoDocumento string `json:"tipoDocumento"`
+        ContenidoDocumento string `json:"contenidoDocumento"`
+}
+
+//Funciones
+
+//co s *SmartContract nos aseguramos que la función sea parte de la 
+// estructura smartcontract
+//ctx: para gestionar la transacción
+//las demas variables de tipo string son propiedades de los activos
+//Esta función permite almacenar en la red blockchain de pjud.
+
+func (s *SmartContract) Set(ctx contractapi.TransactionContextInterface, docId string, demandante string, tipoDocumento string, contenidoDocumento string) error {
+  
+    //Validar Existencia de transacción
+    documento, err := s.Query(ctx, docId)
+
+    if documento != nil{
+        fmt.Printf("docId ya existe, error: %s", err.Error())
+        return err
+    }
+
+    //estructura de go
+    documento := Documento{
+         Demandante: demandante,
+         TipoDocumento: tipoDocumento,
+         ContenidoDocumento: contenidoDocumento,
+    }
+    //transformar a byte al elemento documento
+    documentoAsBytes, err := json.Marshal(documento)
+    if err != nil {
+       fmt.Printf("Marshal error: %s", err.Error())
+       return err
+    }
+    
+    //PutState permite guardar en el libro distribuido el id y valor
+    //el libro es de tipo clave valor distribuido
+    return ctx.GetStub().PutState(foodId, foodAsBytes)
+
+}
+
+//Función para consultar en el ledger o libro mayor.
+
+func (s *SmartContract) Query(ctx contractapi.TransactionContextInterface, documentoId string) (*Documento, error) {
+  
+   //Consultar estado actual esta relacionado con un estampa de tiempo y 
+   //transacción de id
+   //Historial de los activos
+   //estampa de tiempo: guardar un estado de tiempo en blockchain 
+   //se guarda de forma automática
+
+   //Retorna valor en bytes y el contenido del posible error
+   documentoAsBytes, err := ctx.GetStub().GetState(documentoId)
+
+   if err != nil {
+      return nil, fmt.Errorf("No se puede leer del estado mundial. %s", err.Error())
+   }
+
+   if documentoAsBytes == nil {
+      return nil, fmt.Errorf("%s No existe", documentoId)
+   }
+
+   documento := new(Documento)
+   
+   err = json.Unmarshal(documentoAsBytes, documento)
+   if err != nil {
+      return nil, fmt.Errorf("Unmarshal error. %s", err.Error())
+   }
+
+   return documento, nil
+}
+
+func main() {
+    //Inicializar contrato inteligente
+    chaincode, err := contractapi.NewChaincode(new(SmartContract))
+    //Diferente a nulo
+    if err != nil {
+      fmt.Printf("Error al crear chaincode envio-documento-demanda-escrito: %s", err.Error())
+      return
+    }
+
+    //Iniciar contrato inteligente y verificar que no tenga errores
+    if err := chaincode.Start(); err != nil {
+       fmt.Printf("Error starting envio-documento-demanda-escrito chaincode: %s", err.Error())
+    }
+}
---
+++ chaincode/Envio-Documento-Demanda-Escrito/go.mod
@@ -0,0 +1,5 @@
+//modulos que seran usandos en el proyecto
+module github.com/blockchainempresarial/curso-hyperledger-fabric/chaincode/foodcontrol
+//version go 1.13
+//pertenece a hyperledger fabric
+require github.com/hyperledger/fabric-contract-api-go v1.1.0
